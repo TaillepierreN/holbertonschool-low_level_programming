eb2e518dd0cbaafc8ea0d4a770fe8394e41d9009
Nicolas T <taillepierren@gmail.com>
2023-10-11 01:58:40 -0700

    hotfix 5.2

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -2,14 +2,12 @@
 
 /**
  * main - print text
- *
- * Description: Prints "with proper grammar, but the outcome is a piece of art,"
+ * Description: Prints "with proper grammar, but the
+ * outcome is a piece of art,"
  * and return 0
- *
  * Return: Always 0 (Success)
  */
-
-int main()
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);

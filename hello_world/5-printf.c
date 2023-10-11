f2fbb672b407548cde24c390a7a8f3630eecc828
Nicolas T <taillepierren@gmail.com>
2023-10-11 01:35:09 -0700

    5-printf.c

---
+++ hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - print text
+ *
+ * Description: Prints "with proper grammar, but the outcome is a piece of art,"
+ * and return 0
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main()
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}

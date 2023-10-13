4b9f76668f39f35eb969f6ca414a1057f6666e38
Nicolas T <taillepierren@gmail.com>
2023-10-13 01:15:53 -0700

    bettyfix 101-quote.c

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,8 +1,12 @@
 #include<unistd.h>
-
+/**
+ * main - print a string
+ *
+ * Return: 1 (success)
+ */
 
 int main(void)
 {
-	write(1,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }

8760c1edefdf8e6aa9d418c7364c01d5067e108e
Nicolas T <taillepierren@gmail.com>
2023-10-16 03:08:52 -0700

    added 5-print_numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - print 0 through 10
+ *
+ * Return: 0(success)
+ */
+
+int main(void)
+{
+	int nbr;
+
+	for (nbr = 0; nbr < 10; nbr++)
+		printf("%d", nbr);
+	printf("\n");
+	return (0);
+}

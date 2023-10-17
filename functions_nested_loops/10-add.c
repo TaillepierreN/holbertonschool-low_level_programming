805a55f7f9f48c102ca8256b984ac53ce6ee9909
TaillepierreN <taillepierren@gmail.com>
2023-10-17 07:16:21 -0700

    added 10-add.c

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - add two integers and return the results
+ *@a: first int
+ *@b: second int
+ * Return: result
+ */
+
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+	return (c);
+}

5441ed6cdf14e73587b909065e267365b9c38f31
TaillepierreN <taillepierren@gmail.com>
2023-10-17 00:15:24 -0700

    added 2-alphabet_x10

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+  * main - print alphabet x10
+  *
+  * Return: 0 success
+  */
+int print_alphabet_x10(void)
+{
+	int i;
+	char ltr;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (ltr = 'a'; ltr <= 'z'; ltr++)
+			_putchar(ltr);
+		_putchar('\n');
+	}
+	return (0);
+}

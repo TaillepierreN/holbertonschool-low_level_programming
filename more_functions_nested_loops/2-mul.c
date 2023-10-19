8b644b4804bfce8ef88970d3dd453179dbb93380
TaillepierreN <taillepierren@gmail.com>
2023-10-19 01:05:39 -0700

    added 2-mul

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - multiply two int
+ * @a: first int to multiply
+ * @b: second int to multiply
+ * Return: result
+ */
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+	return (c);
+}

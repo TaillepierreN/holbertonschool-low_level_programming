16fd681740bc97af912c04e870ac4c368ec38aab
TaillepierreN <taillepierren@gmail.com>
2023-10-20 02:10:10 -0700

    added 7-print_diagonal.c

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_diagonal - print n time \
+ *
+ * @n: number of prints
+ *
+ * Return: void
+ */
+void print_diagonal(int n)
+{
+	int i, y;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (y = 0; y < i; y++)
+
+				_putchar(' ');
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+}

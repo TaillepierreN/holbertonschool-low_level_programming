bbd1b24071ba9d741c6f159586e02fc702af3a26
TaillepierreN <taillepierren@gmail.com>
2023-10-22 23:49:47 -0700

    betty hotfix 0.1

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * reset_to_98 takes a pointer and updates the value it points to to 98
- * @*n: address of the value to change
+ * reset_to_98 - takes a pointer and updates the value it points to to 98
+ * @n: address of the value to change
  * Return void
  */
 

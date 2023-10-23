6b002372bef5e6a7688187b6bf08b59bc79f8967
TaillepierreN <taillepierren@gmail.com>
2023-10-23 04:11:45 -0700

    added 7-puts_half

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * puts_half print the second half of a string
+ * @str: string to print
+ */
+
+void puts_half(char *str)
+{
+	int i = 0, y;
+
+	while (str[i] != '\0')
+		i++;
+
+	if (i % 2 == 1)
+		y = i / 2;
+	else
+		y = (i - 1) / 2;
+	
+	while (y < i)
+	{
+		_putchar(str[y]);
+		y++;
+	}
+	_putchar('\n');
+}

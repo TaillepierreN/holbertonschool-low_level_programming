3a58120be125661c89490b88024b5675b01ab2a8
TaillepierreN <taillepierren@gmail.com>
2023-10-23 01:07:04 -0700

    added 2-strlen

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- * _strlen return the length of a string
+ * _strlen - return the length of a string
  * @s: string to find the length of
  * Return: int length
  */
 
 int _strlen(char *s)
 {
-	int length;
+	int length = 0;
 
-	for (length = 0; s[length] != '\0'; ++length);
-
+	while (s[length] != '\0')
+		++length;
 	return (length);
 }

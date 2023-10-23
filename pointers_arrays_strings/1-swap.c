f68b368cd4b05b60c288fc58511ec54ad48483b4
TaillepierreN <taillepierren@gmail.com>
2023-10-23 00:30:41 -0700

    added 1-swap

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps values of two int
+ * @a: first int to swap
+ * @b: second int to swap
+ */
+
+void swap_int(int *a,int *b)
+{
+	int c;
+
+	c = *b;
+	*b = *a;
+	*a = c;
+}

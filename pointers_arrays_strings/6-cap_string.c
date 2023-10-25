d9da0b89b68da644029f5a536443626bfffb56bc
TaillepierreN <taillepierren@gmail.com>
2023-10-25 01:58:02 -0700

    hotfix 6.1

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -9,17 +9,20 @@
 char *cap_string(char *str)
 {
 	int i = 0, j;
-	char separator[] = {',', ';', '.', '!', '?','"', '(', ')', '{', '}', ' ', '\n', '\t'};
+	char sep[] = {',', ';', '.', '!', '?',
+		'"', '(', ')', '{', '}', ' ', '\n', '\t'};
 
-	
 	while (str[i] != '\0')
 	{
+		if (i == 0 && (str[i] >= 'a' && str[i] <= 'z'))
+			str[i] -= 32;
+
 		for (j = 0; j < 13; j++)
 		{
-			if (str[i] == separator[j])
+			if (str[i] == sep[j])
 			{
-				if (str[i+1] >= 'a' && str[i+1] <= 'z')
-					str[i+1] -= 32;
+				if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
+					str[i + 1] -= 32;
 			}
 		}
 	i++;

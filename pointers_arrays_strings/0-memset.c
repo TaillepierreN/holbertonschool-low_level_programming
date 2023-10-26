9ca2d80634b0266a756b666e53d145a4667332f6
TaillepierreN <taillepierren@gmail.com>
2023-10-26 00:34:43 -0700

    added 0-memset

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memset - fills the first n bytes of the memory area pointed to by s with the byte b
+ * @s: area to fill
+ * @b: byte to fill with
+ * @n: number of byte to fill
+ * Return: memory area filled
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		s[i] = b;
+		i++;
+	}
+	return (s);
+}

b5610e33de137143f00e4170e6b62b041fd1765c
TaillepierreN <taillepierren@gmail.com>
2023-10-27 08:44:47 -0700

    added 5-strstr

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strstr - locate a substring
+ * @haystack: string to look into
+ * @needle: string to look for
+ * Return: pointer to the begining of the needle or null
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+
+	int i, j;
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		if (haystack[i] == needle[0])
+		{
+			for (j = 0; needle[j] != '\0'; j++)
+			{
+				if (needle[j] != haystack[i + j])
+				{
+					break;
+				}
+			}
+			if (needle[j] == '\0')
+			{
+				return (&haystack[i]);
+			}
+		}
+	}
+		return (NULL);
+}

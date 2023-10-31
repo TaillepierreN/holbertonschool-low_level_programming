909da7076be6330d39e45986f4811f5cd1db38fd
TaillepierreN <taillepierren@gmail.com>
2023-10-31 05:42:48 -0700

    added 3-mul

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * main - multiplies two numbers
+ * @argc: argument count
+ * @argv: argument value
+ * Return: 0 (success)
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
+}

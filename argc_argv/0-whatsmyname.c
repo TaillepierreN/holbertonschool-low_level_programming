b58731553686c2c407fbf91f31f9ce3ea8a68dc2
TaillepierreN <taillepierren@gmail.com>
2023-10-31 05:00:33 -0700

    added setup + 0-whatsmyname

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - print the name of the program even if it's not slim shady
+ * @argc: argument count
+ * @argv: argument value
+ */
+
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}

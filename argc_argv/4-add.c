a08db5ecb59691ccb28299de02bf2234b5e362c0
TaillepierreN <taillepierren@gmail.com>
2023-10-31 06:17:13 -0700

    added 4-add

---
+++ argc_argv/4-add.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - add positive numbers
+ * @argc: argument count
+ * @argv: argument value
+ * Return 0 (success) or 1 (error)
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, sum = 0;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		if(*argv[i] >= '0' && *argv[i] <= '9')
+		{
+			sum += atoi(argv[i]);
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}

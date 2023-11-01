4c1cc7725acc57a9dacb9d63d616e9d1f01f845b
TaillepierreN <taillepierren@gmail.com>
2023-11-01 01:45:16 -0700

    hotfix 5.1

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -11,6 +11,13 @@ int _sqrt_recursion(int n)
 	return (_sqrt_recursion_with_guess(n, n / 2));
 }
 
+/**
+ * _sqrt_recursion_with_guess - calculate the natural root of a number
+ * @n: number to get the square root of
+ * @guess: guess needed to check if it is the square root
+ * Return: int square root
+ */
+
 int _sqrt_recursion_with_guess(int n, int guess)
 {
 	int new_guess;
@@ -18,7 +25,7 @@ int _sqrt_recursion_with_guess(int n, int guess)
 	if (n < 0)
 		return (-1);
 	if (n == 0 || n == 1)
-		return (n);	
+		return (n);
 
 	new_guess = (guess + n / guess) / 2;
 	if (new_guess == guess || new_guess == guess + 1)
@@ -26,8 +33,6 @@ int _sqrt_recursion_with_guess(int n, int guess)
 		if (guess * guess == n)
 			return (guess);
 		else
-			return(-1);
-
+			return (-1);
 	}
-		return _sqrt_recursion_with_guess(n, new_guess);
-}
+		return (_sqrt_recursion_with_guess(n, new_guess));

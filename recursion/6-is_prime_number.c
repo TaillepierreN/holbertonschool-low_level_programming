2d9eebe694145491d8c18d7dec695a0714bde833
TaillepierreN <taillepierren@gmail.com>
2023-11-01 01:53:46 -0700

    hotfix 6.1

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -9,26 +9,33 @@
 int is_prime_number(int n)
 {
 	int i;
-	if (n <=0 )
+
+	if (n <= 0)
 		return (0);
 	i = n / 2;
-	
 	return (check_if_prime(n, i));
 }
+
+/**
+ * check_if_prime - check if int is prime number
+ * @n: int to check
+ * @i: int to check with
+ * Return: 1 if prime, else 0
+ */
+
 int check_if_prime(int n, int i)
 {
 	if (i == 0 || n == 0)
 	{
 		return (0);
-	
 	}
 	else if (i == 1)
 	{
-		return 1;
+		return (1);
 	}
 	else if (n % i == 0)
 	{
-		return 0;
+		return (0);
 	}
 	else
 	{

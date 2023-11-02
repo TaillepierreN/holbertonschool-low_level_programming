35d960dbebc35ccae26b0e3e46eb4b9d27a9ec79
TaillepierreN <taillepierren@gmail.com>
2023-11-02 05:21:13 -0700

    hotfix 17.8.2 wrong file pushed

--- recursion/101-wildcmp.c
+++ recursion/101-wildcmp.c
@@ -1,21 +1,3 @@
-        if (*s1 == 0 && *s2 == 0)
-        {
-                return (1);
-        }
-        if (*s1 == *s2)
-                return (wildcmp(s1 + 1, s2 + 1));
-
-        if (*s2 == '*')
-        {
-                if (*(s2 + 1) == '*')
-                        return (wildcmp(s1, s2 + 1));
-
-                if (*(s2 + 1) != 0 && *s1 == 0)
-                        return (0);
-                if (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2))
-                        return (1);
-        }
-        return (0);
 #include "main.h"
 
 /**

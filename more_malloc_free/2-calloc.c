10057623be2016fa570048c8e15cbcf220f622ba
TaillepierreN <taillepierren@gmail.com>
2023-11-08 03:21:52 -0800

    hotfix 21.2.1

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -12,11 +12,11 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	char *array;
 	unsigned int i;
 
+	if(nmemb == 0 || size == 0)
+		return (0);
 	array = malloc(nmemb * size);
-	if (array == NULL || nmemb == 0 || size == 0)
-	{
+	if (array == NULL)
 		return (NULL);
-	}
 	for (i = 0; i < nmemb; i++)
 		array[i] = 0;
 	return (array);

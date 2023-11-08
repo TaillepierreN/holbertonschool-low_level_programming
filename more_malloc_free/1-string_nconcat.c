1f3242d729ebd5e48c8e13d0d48b41877b60f900
TaillepierreN <taillepierren@gmail.com>
2023-11-08 00:56:02 -0800

    21.1.2

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -13,25 +13,23 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int i, j;
 	char *newstring;
 
-	if (s1 == NULL)
-		s1 = "";
-
-	if (s2 == NULL)
-		s2 = "";
-
 	newstring = malloc(((strlen(s1) + strlen(s2) + 1) * sizeof(char)));
-	
+
 	if (newstring == NULL)
 		return (NULL);
-	
+
 	for (i = 0; i < strlen(s1); i++)
 	{
+		if (s1[0] == 0)
+			newstring[0] = '\0';
 		newstring[i] = s1[i];
 	}
 	if (n >= strlen(s2))
 	{
 		for (j = 0; j < strlen(s2); j++)
 		{
+			if (s2[0] == 0)
+				newstring[i] = '\0';
 			newstring[i] = s2[j];
 			i++;
 		}
@@ -40,6 +38,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		for (j = 0; j < n; j++)
 		{
+			if (s2[0] == 0)
+				newstring[i] = '\0';
 			newstring[i] = s2[j];
 			i++;
 		}

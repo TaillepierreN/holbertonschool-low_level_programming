02fb1afd97e96d60ebfd49cd291f8a4020f49e89
TaillepierreN <taillepierren@gmail.com>
2023-11-10 03:45:11 -0800

    added 20.5.0 - 100-argstostr

---
+++ malloc_free/100-argstostr.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * argstostr - concatenates all the arguments of the program
+ * @ac: argument count
+ * @av: argument value
+ * Return: string concatenated
+ */
+
+char *argstostr(int ac, char **av)
+{
+	int i = 0, j, totallength = 0, k = 0;
+	char *string;
+	
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	while (i < ac)
+	{
+		j = 0;
+		while (av[i][j] != '\0')
+		{
+			totallength++;
+			j++;
+		}
+		i++;
+	}
+	totallength = totallength + 1 + ac;
+	string = malloc(totallength * sizeof(char));
+	if (string == NULL)
+		return (NULL);
+	i = 0;
+	while (i < ac)
+	{
+		j = 0;
+		while(av[i][j] != '\0')
+		{
+			string[k] = av[i][j];
+			j++;
+			k++;
+		}
+		string[k] = '\n';
+		k++;
+		i++;
+	}
+	return (string);
+}

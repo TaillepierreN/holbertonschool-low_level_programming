f80f1c4baf49868460a9f60970736a191d022463
TaillepierreN <taillepierren@gmail.com>
2023-11-13 05:05:31 -0800

    betty fixup

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -1,20 +1,25 @@
 #include "dog.h"
 
+/**
+ * print_dog - print the data on the dog
+ * @d: pointer to the struct where data is stored
+ */
+
 void print_dog(struct dog *d)
 {
 	if (d != NULL)
 	{
-		if((*d).name == NULL)
+		if ((*d).name == NULL)
 			printf("Name: (nil)\n");
 		else
 			printf("Name: %s\n", d->name);
-		if((*d).age == '\0')
+		if ((*d).age == '\0')
 			printf("Age: (nil)\n");
 		else
 			printf("Age: %f\n", d->age);
-		if((*d).owner == NULL)
+		if ((*d).owner == NULL)
 			printf("Owner: (nil)\n");
 		else
-			printf("Owner: %s\n",d->owner);
+			printf("Owner: %s\n", d->owner);
 	}
 }

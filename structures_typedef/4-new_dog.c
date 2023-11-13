12ed249f743889f2462862d1c574cfe3e5bd3383
TaillepierreN <taillepierren@gmail.com>
2023-11-13 06:50:44 -0800

    freedom for malloc!

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,14 +1,26 @@
 #include "dog.h"
 
+/**
+ * new_dog - add a new dog
+ * @name: of the dog
+ * @age: of the dog
+ * @owner: of the dog
+ * Return: a pointer to the struct new dog
+ */
+
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *doggo = malloc(sizeof(dog_t));
 
+	if (doggo == NULL)
+	{
+		free(doggo);
+		return (NULL);
+	}
+	
 	doggo->name = name;
 	doggo->age = age;
 	doggo->owner = owner;
 
-	if (doggo == NULL)
-		return (NULL);
 	return (doggo);
 }

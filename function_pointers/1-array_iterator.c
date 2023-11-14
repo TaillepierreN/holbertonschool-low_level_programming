b75fbcb66815ab1647d04c4d112fd9effa9b0e81
TaillepierreN <taillepierren@gmail.com>
2023-11-14 02:24:41 -0800

    hotfix 24.1.1

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -1,11 +1,19 @@
 
 #include "function_pointers.h"
 
+/**
+ * array_iterator - execute a function given
+ * as param on each element of an array
+ * @array: pointer to the array
+ * @size: size of the array
+ * @action: function to execute on the array
+ */
+
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i = 0;
 
-	while(i < size)
+	while (i < size)
 	{
 		action(array[i]);
 		i++;

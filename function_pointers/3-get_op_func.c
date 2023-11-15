8ca917e2653cdee0b92559319301e360cb3ab681
TaillepierreN <taillepierren@gmail.com>
2023-11-15 05:01:47 -0800

    hotfix 24.3.2

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -21,7 +21,8 @@ int (*get_op_func(char *s))(int, int)
 
 	while (i < 5)
 	{
-		if (s[0] == *(ops[i].op))
+		if (*ops[i].op == *s && s[1] == '\0')	
+		/*if (s[0] == *(ops[i].op))*/
 			return (ops[i].f);
 		i++;
 	}

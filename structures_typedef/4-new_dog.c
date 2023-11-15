3880264f8ef5cb7c900dc76c8b990e9ec2b2ccc9
TaillepierreN <taillepierren@gmail.com>
2023-11-15 02:06:50 -0800

    hotfix 23.4.3

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -17,6 +17,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(doggo);
 		return (NULL);
 	}
+	doggo->name = _strdup(name);
+	doggo->age = age;
+	doggo->owner = _strdup(owner);
 
 	if (name == NULL || owner == NULL)
 	{
@@ -27,9 +30,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 	}
 
 
-	doggo->name = _strdup(name);
-	doggo->age = age;
-	doggo->owner = _strdup(owner);
 
 	return (doggo);
 }

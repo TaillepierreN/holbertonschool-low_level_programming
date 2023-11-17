0ec6c4239f8416149a203cacfdb6b5b47f0aacd2
TaillepierreN <taillepierren@gmail.com>
2023-11-17 02:21:47 -0800

    update variadic_functions

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -5,9 +5,19 @@
 #include <stdlib.h>
 #include <stdarg.h>
 
+typedef struct datatypes
+{
+	char charac;
+	void (*func)(va_list);
+} datatype_list;
+
 int sum_them_all(const unsigned int n, ...);
 
 void print_numbers(const char *separator, const unsigned int n, ...);
+
+void print_strings(const char *separator, const unsigned int n, ...);
+
+void print_all(const char * const format, ...);
 
 void print_strings(const char *separator, const unsigned int n, ...);
 

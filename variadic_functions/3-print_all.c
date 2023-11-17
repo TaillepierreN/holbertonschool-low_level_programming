c5ffd597bd388311264112bc44c6dd8861ca7179
TaillepierreN <taillepierren@gmail.com>
2023-11-17 02:51:41 -0800

    hotfix 25.3.3

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,25 +1,41 @@
 #include "variadic_functions.h"
 
+/**
+ * print_char - print characters
+ * @data_list: list of char arguments to print
+ */
 
 void print_char(va_list data_list)
 {
 	printf("%c", va_arg(data_list, int));
 }
 
+/**
+ * print_int - print integers
+ * @data_list: list of int arguments to print
+ */
 void print_int(va_list data_list)
 {
 	printf("%d", va_arg(data_list, int));
 }
 
+/**
+ * print_float - print floats
+ * @data_list: list of float arguments to print
+ */
 void print_float(va_list data_list)
 {
 	printf("%f", va_arg(data_list, double));
 }
 
+/**
+ * print_string - print strings
+ * @data_list: list of strings arguments to print
+ */
 void print_string(va_list data_list)
 {
 	char *string;
-	
+
 	string = va_arg(data_list, char *);
 	if (!string)
 	{
@@ -28,10 +44,14 @@ void print_string(va_list data_list)
 	printf("%s", string);
 }
 
+/**
+ * print_all - print arguments passed to the function
+ * @format: chars determining the type of argument to print
+ * @...: arguments to print
+ */
 void print_all(const char * const format, ...)
 {
-	datatype_list datatypes[] =
-	{
+	datatype_list datatypes[] = {
 		{'c', print_char},
 		{'i', print_int},
 		{'f', print_float},

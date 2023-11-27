5d4b79b88cab30357c9707f690d3aa730fb254fc
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2023-11-27 11:21:55 +0100

    0-print_list, added the whole function

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,1 +1,23 @@
-#include "list.h"
+#include "lists.h"
+
+/**
+ * print_list - print each element of the list and number of elements
+ * @h: adress of the first node
+ * Return: unsigned int number of elements
+*/
+
+size_t print_list(const list_t *h)
+{
+    unsigned int nbrelem = 0;
+
+    while (h != NULL)
+    {
+        if (h->str == NULL)
+            printf("[0] (nil)\n");
+        else
+            printf("[%u] %s\n", h->len, h->str);
+        nbrelem++;
+        h = h->next;
+    }
+    return(nbrelem);
+}

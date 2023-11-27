90856d4e5ef749b763d15c324a6978e4f0ac6c4c
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2023-11-27 15:48:59 +0100

    fix 4-free_list, removed useless frees

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -4,14 +4,12 @@ void free_list(list_t *head)
 {
     list_t *temp, *bin;
 
-    temp = head->next;
-
-    free(head);
+    temp = head;
     while (temp != NULL)
     {
         bin = temp;
         temp = temp->next;
         free(bin);
     }
-    free(temp);
+
 }

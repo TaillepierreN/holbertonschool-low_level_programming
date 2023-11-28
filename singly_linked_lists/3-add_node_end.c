4ac7d5119cafd01a85c154bcec73418fa1645212
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2023-11-28 10:33:36 +0100

    3- add_node_end removed redundency

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -18,13 +18,7 @@ list_t *add_node_end(list_t **head, const char *str)
         *head = newList;
         return (*head);
     }
-    last = (*head)->next;
     last = *head;
-    if (last == NULL)
-    {
-        *head = newList;
-        return *head;
-    }
     while (last->next != NULL)
         last = last->next;
     last->next = newList;

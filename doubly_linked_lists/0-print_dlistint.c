25e3f6d60a450548c39ea2a2798b2d1d047672a3
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2023-11-28 10:05:46 +0100

    added 0-print_dlistint

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	unsigned int nbrelem = 0;
+
+	while (h->prev != NULL)
+	{
+		h = h->prev;
+	}
+	
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		nbrelem++;
+		h = h->next;
+	}
+	
+	return (nbrelem);
+}

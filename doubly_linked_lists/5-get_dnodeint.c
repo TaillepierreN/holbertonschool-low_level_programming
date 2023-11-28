3b51eeae0e250226b6697bbec7cc11f402c8ea38
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2023-11-28 10:54:17 +0100

    5-get_dnodeint fix condition causing segmentation fault in while loop

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -4,12 +4,14 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	unsigned int i = 0;
 
+	if (head == NULL)
+		return (NULL);
 	while (head->prev != NULL)
 	{
 		head = head->prev;
 	}
 
-	while (head->next != NULL || i != index)
+	while (head->next != NULL && i != index)
 	{
 		head = head->next;
 		i++;

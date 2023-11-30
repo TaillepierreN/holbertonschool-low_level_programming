20dd81c0d35c374e96411ed12ba1523a37bee196
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2023-11-30 15:33:04 +0100

    fix betty

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -9,12 +9,12 @@
 
 int _pow_recursion(int x, int y)
 {
-    if (y < 0)
-        return (-1);
-    else if (y == 0)
-        return (1);
-    else
-        return (x * _pow_recursion(x, y - 1));
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	else
+		return (x * _pow_recursion(x, y - 1));
 }
 
 /**
@@ -26,22 +26,22 @@ int _pow_recursion(int x, int y)
  */
 unsigned int binary_to_uint(const char *b)
 {
-    int i = 0, strlength = 0;
-    unsigned int sum = 0;
+	int i = 0, strlength = 0;
+	unsigned int sum = 0;
 
-    if (b == NULL)
-        return (0);
+	if (b == NULL)
+		return (0);
 
-    strlength = strlen(b);
-    while (b[i] != '\0')
-    {
-        if (b[i] > '1' || b[i] < '0')
-            return (0);
-        else if (b[i] == '1')
-        {
-            sum += _pow_recursion(2, (strlength - (i + 1)));
-        }
-        i++;
-    }
-    return (sum);
+	strlength = strlen(b);
+	while (b[i] != '\0')
+	{
+		if (b[i] > '1' || b[i] < '0')
+			return (0);
+		else if (b[i] == '1')
+		{
+			sum += _pow_recursion(2, (strlength - (i + 1)));
+		}
+		i++;
+	}
+	return (sum);
 }

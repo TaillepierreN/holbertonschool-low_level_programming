07b6e27b27be2f3497ba4566d0d82f5a00039d96
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2023-11-30 15:27:12 +0100

    added comment

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+/**
+ * _pow_recursion - return the value of x raised to the power of y
+ * @x: int to be raised
+ * @y: int to raised to the power of
+ * Return: value raised, if y is lower than 0 return -1
+ */
+
 int _pow_recursion(int x, int y)
 {
     if (y < 0)
@@ -9,6 +16,14 @@ int _pow_recursion(int x, int y)
     else
         return (x * _pow_recursion(x, y - 1));
 }
+
+/**
+ * binary_to_uint - converts a string containing binary into an 
+ * unsigned int
+ * @b: string containing the binary
+ * Return: converted number, 0 if fail
+ * 
+*/
 unsigned int binary_to_uint(const char *b)
 {
     int i = 0, strlength = 0;

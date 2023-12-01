9d7b4eb2e15d2cbc1ef2fbab0d3924b569afcb98
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2023-12-01 11:46:46 +0100

    2-get_bit replace space with tabs, and _putchar with  putchar

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -2,26 +2,26 @@
 
 int get_bit_at_index_rec(unsigned long int n, unsigned int index, unsigned int i)
 {
-    if (i == index)
-    {
-        _putchar('0' + (n & 1));
-        return (n & 1);
-    }
-    else
-    {
-        i--;
-        return (get_bit_at_index_rec(n, index, i));
-    }
+	if (i == index)
+	{
+		putchar('0' + (n & 1));
+		return (n & 1);
+	}
+	else
+	{
+		i--;
+		return (get_bit_at_index_rec(n, index, i));
+	}
 }
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-    unsigned int i = 1, divisor = 1;
+	unsigned int i = 1, divisor = 1;
 
-    while (index / divisor > 9)
-    {
-        divisor *= 10;
-        i++;
-    }
-    return (get_bit_at_index_rec(n, index, i));
+	while (index / divisor > 9)
+	{
+		divisor *= 10;
+		i++;
+	}
+	return (get_bit_at_index_rec(n, index, i));
 }

c450c6c45e600b7df8e1c17c845e1f33eb7d8719
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2023-12-01 12:19:10 +0100

    2-get_bitadded more comment

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+/**
+ * get_bit_at_index_rec - go through the number to get to index
+ * @n: number
+ * @index: index to go to
+ * @i: iteration index
+ * Return: value of bit at index, -1 if error
+*/
 int get_bit_at_index_rec(unsigned long int n,
 unsigned int index, unsigned int i)
 {
@@ -7,6 +14,8 @@ unsigned int index, unsigned int i)
 	{
 		return (n & 1);
 	}
+	else if (n == 0 && i != index)
+		return (-1);
 	else
 	{
 		i++;
@@ -14,6 +23,12 @@ unsigned int index, unsigned int i)
 	}
 }
 
+/**
+ * get_bit - initialise i and call recursive
+ * @n: number to find the bit value of
+ * @index: index where to find the bit
+ * Return: value of bit at index, -1 if error
+*/
 int get_bit(unsigned long int n, unsigned int index)
 {
 	int i = 0;

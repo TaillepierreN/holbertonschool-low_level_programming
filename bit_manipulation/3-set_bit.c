68d04828a8596e48c5858a86e2db2c7bdb50b0cc
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2023-12-01 14:35:30 +0100

    3-set_bit forgot to multiply sizeof(unsigned long int) by 8 to check if index is out of range

--- bit_manipulation/3-set_bit.c
+++ bit_manipulation/3-set_bit.c
@@ -11,9 +11,9 @@ int set_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned int i = 1;
 
-	if (n == NULL || index >= sizeof(unsigned long int))
+	if (n == NULL || index >= sizeof(unsigned long int) * 8);
 		return (-1);
-			
+
 	*n = i << index | *n;
 	return (1);
 }

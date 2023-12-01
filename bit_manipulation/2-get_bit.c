2e4a834cf23a20960ad18cbbe0e027e12375b2d9
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2023-12-01 12:07:07 +0100

    2-get_bit remove putchar since it's printed in the main, removed the divisor part(useless) and change iteration counting up instead of down

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -4,24 +4,17 @@ int get_bit_at_index_rec(unsigned long int n, unsigned int index, unsigned int i
 {
 	if (i == index)
 	{
-		putchar('0' + (n & 1));
 		return (n & 1);
 	}
 	else
 	{
-		i--;
-		return (get_bit_at_index_rec(n, index, i));
+		i++;
+		return (get_bit_at_index_rec(n>>1, index, i));
 	}
 }
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned int i = 1, divisor = 1;
-
-	while (index / divisor > 9)
-	{
-		divisor *= 10;
-		i++;
-	}
+	int i = 0;
 	return (get_bit_at_index_rec(n, index, i));
 }

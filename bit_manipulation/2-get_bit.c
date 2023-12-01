30e02b536c7da0c1024090b87350cf62fe4465b4
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2023-12-01 12:16:36 +0100

    2-get_bit making it betty compliant, added comments, added check if

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -1,6 +1,7 @@
 #include "main.h"
 
-int get_bit_at_index_rec(unsigned long int n, unsigned int index, unsigned int i)
+int get_bit_at_index_rec(unsigned long int n,
+unsigned int index, unsigned int i)
 {
 	if (i == index)
 	{
@@ -9,12 +10,13 @@ int get_bit_at_index_rec(unsigned long int n, unsigned int index, unsigned int i
 	else
 	{
 		i++;
-		return (get_bit_at_index_rec(n>>1, index, i));
+		return (get_bit_at_index_rec(n >> 1, index, i));
 	}
 }
 
 int get_bit(unsigned long int n, unsigned int index)
 {
 	int i = 0;
+
 	return (get_bit_at_index_rec(n, index, i));
 }

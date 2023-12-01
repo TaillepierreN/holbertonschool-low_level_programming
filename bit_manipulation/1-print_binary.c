c2a71873ec8ee9b1af8b797ff1c6317446e7855b
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2023-12-01 10:33:06 +0100

    1-print_binary can't use % and / , i need to read better, switched to >> right shift and bitwise &

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -23,7 +23,7 @@ void print_binary_rec(unsigned long int n)
 {
 	if (n != 0)
 	{
-		print_binary_rec(n / 2);
-		_putchar('0' + (n % 2));
+		print_binary_rec(n >> 1);
+		_putchar('0' + (n & 1));
 	}
 }

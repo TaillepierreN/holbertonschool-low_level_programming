d663cff2e27d9953eb48bf24cd722c23b6299a55
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2023-12-01 14:28:43 +0100

    added 3-set_bit

---
+++ bit_manipulation/3-set_bit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * set_bit - set the bit at index to 1
+ * @n: value where to set the bit
+ * @index: index of the bit to set
+ * Return: 1 if success else -1
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int i = 1;
+
+	*n = i << index | *n;
+	return (1);
+}

545021398890c228d941ad441fff7a79b1760950
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2023-12-01 14:00:16 +0100

    2-get_bit complete rework of get_bit,with no recursivity n rightshift to index and print the bit

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -1,37 +1,15 @@
 #include "main.h"
 
 /**
- * get_bit_at_index_rec - go through the number to get to index
- * @n: number
- * @index: index to go to
- * @i: iteration index
- * Return: value of bit at index, -1 if error
-*/
-int get_bit_at_index_rec(unsigned long int n,
-unsigned int index, unsigned int i)
-{
-	if (i == index)
-	{
-		return (n & 1);
-	}
-	else if (n == 0 && i != index)
-		return (-1);
-	else
-	{
-		i++;
-		return (get_bit_at_index_rec(n >> 1, index, i));
-	}
-}
-
-/**
- * get_bit - initialise i and call recursive
+ * get_bit - return value of a bit at index
  * @n: number to find the bit value of
  * @index: index where to find the bit
  * Return: value of bit at index, -1 if error
-*/
+ */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	int i = 0;
+	if (index >= sizeof(unsigned long int) * 8)
+		return -1;
 
-	return (get_bit_at_index_rec(n, index, i));
+	return ((n >> index) & 1);
 }

cf3a9b20223ed080e6ed43de42cbf7060dc87973
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2023-12-01 17:50:58 +0100

    100-first.c added comment

--- singly_linked_lists/100-first.c
+++ singly_linked_lists/100-first.c
@@ -1,5 +1,9 @@
 #include "lists.h"
 
+/**
+ * print_first - print text before the main() is executed
+ */
+
 void __attribute__((constructor)) print_first(void)
 {
 	printf("You're beat! and yet, you must allow,\n");

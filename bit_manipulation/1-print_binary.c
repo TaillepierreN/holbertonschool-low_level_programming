10821747b092e9137791839379915c3c32db4189
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2023-12-01 10:06:13 +0100

    added 1-print_binary

---
+++ bit_manipulation/1-print_binary.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+void print_binary(unsigned long n)
+{
+    if (n == 0)
+    {
+        _putchar('0');
+    }
+    else
+    {
+        print_binary_rec(n);
+    }
+}
+
+void print_binary_rec(unsigned long int n)
+{
+    if (n != 0)
+    {
+        print_binary_rec(n / 2);
+        _putchar('0' + (n % 2));
+    }
+}

efdbfb5a59f9d9de38631ee6899fbf202b5addd0
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2023-12-04 10:51:46 +0100

    0-read_textfile malloced the buffer

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -13,12 +13,12 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
     int fd;
     ssize_t nbr_written;
-    char *buffer[letters];
+    char *buffer;
 
     if (!filename)
         return (0);
     fd = open(filename, O_RDONLY);
-
+    buffer = malloc(letters);
     if (fd == -1)
         return (0);
     read(fd, buffer, sizeof(buffer));

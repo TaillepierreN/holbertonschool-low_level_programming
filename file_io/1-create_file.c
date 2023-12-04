6702d9744e3b1f3f44d5a63693c680efd933244f
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2023-12-04 13:14:28 +0100

    1-create_file added file closing where needed

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -21,7 +21,10 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	file_written = write(file_created, text_content, strlen(text_content));
 	if (file_written == -1)
+	{
+		close(file_created);
 		return (-1);
-
+	}
+	close(file_created);
 	return (1);
 }

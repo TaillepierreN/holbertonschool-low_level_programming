88ed6b7c5feb4032d959b1b0e6e65d76e278e2e3
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2023-12-04 13:22:56 +0100

    1-create_file added O_WRONLY in the creation of the file to be able to write in

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -16,13 +16,12 @@ int create_file(const char *filename, char *text_content)
 	if (!text_content)
 		text_content = "";
 
-	file_created = open(filename, O_CREAT | O_TRUNC, 0600);
+	file_created = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (file_created == -1)
 		return (-1);
 	file_written = write(file_created, text_content, strlen(text_content));
 	if (file_written == -1)
 	{
-		perror("error");
 		close(file_created);
 		return (-1);
 	}

9f78572e698074aeb3272d48a1063004287a21ab
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2023-12-04 11:05:19 +0100

    0-read_textfile change sizeof letter

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -5,28 +5,32 @@
  * @filename: file to read
  * @letters: number of letters it should read and print
  * Return: 0 if filename is NULL,if file can't be opened and read,if write fail
- * or does not write the expected amount of bytes else return number of 
+ * or does not write the expected amount of bytes else return number of
  * letters it could read and print
-*/
+ */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    int fd;
-    ssize_t nbr_read, nbr_written;
-    char *buffer;
+	int fd;
+	ssize_t nbr_read, nbr_written;
+	char *buffer;
 
-    if (!filename)
-        return (0);
-    fd = open(filename, O_RDONLY);
-    buffer = malloc(letters * sizeof(char));
-    if (fd == -1)
-        return (0);
-    nbr_read = read(fd, buffer, sizeof(letters));
-    nbr_written = write(STDOUT_FILENO, buffer, nbr_read);
+	if (!filename)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	buffer = malloc(letters * sizeof(char));
+	if (!buffer)
+	{
+		close(fd);
+		return (0);
+	}
+	nbr_read = read(fd, buffer, letters);
+	nbr_written = write(STDOUT_FILENO, buffer, nbr_read);
 
-    if (nbr_written <= 0)
-        return (0);
+	if (nbr_written <= 0)
+		return (0);
 
-    return (nbr_written);
-
+	return (nbr_written);
 }

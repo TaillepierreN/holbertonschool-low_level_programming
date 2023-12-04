41cd5c9a005638d43594be2c55c35741dd070a26
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2023-12-04 13:06:34 +0100

    created 1-create_file and updated main.h with string.h

---
+++ file_io/1-create_file.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * create_file - create a file
+ * @filename: name of file to create
+ * @text_content: string to write in the file
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int file_created, file_written;
+
+	if (!filename)
+		return (-1);
+
+	if (!text_content)
+		text_content = "";
+
+	file_created = open(filename, O_CREAT | O_TRUNC, 0600);
+	if (file_created == -1)
+		return (-1);
+	file_written = write(file_created, text_content, strlen(text_content));
+	if (file_written == -1)
+		return (-1);
+
+	return (1);
+}

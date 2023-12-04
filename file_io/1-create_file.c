fe2f74985c32d279b90438b4fd8a4cf596d7c124
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2023-12-04 13:16:10 +0100

    1-create_file debugging

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -22,6 +22,7 @@ int create_file(const char *filename, char *text_content)
 	file_written = write(file_created, text_content, strlen(text_content));
 	if (file_written == -1)
 	{
+		perror("error");
 		close(file_created);
 		return (-1);
 	}

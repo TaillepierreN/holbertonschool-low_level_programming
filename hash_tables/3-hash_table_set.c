9ad763fcc01456dfc67b371b63ca601b66f179a9
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2023-12-05 12:24:00 +0100

    3-hash_table_set free the new element if part of malloc fail / copy values in new element / switch key to unsigned key for the key_index / check if key is null / add new element to hash table,with collision in mind

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -18,20 +18,35 @@ hash_node_t *create_element(const char *key, const char *value)
 	element->value = malloc(strlen(value) + 1);
 	if (!element->key || !element->value)
 	{
+		free(element->key);
+		free(element->value);
 		free(element);
 		return (NULL);
 	}
+	strcpy(element->key, key);
+	strcpy(element->value, value);
 	element->next = NULL;
+	return (element);
 }
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
-	hash_node_t *new_element, cu;
+	unsigned char *unsigned_key;
+	hash_node_t *new_element, *current_element;
 
-	index = key_index(key, ht->size);
-
+	if (key[0] == '\0')
+		return (0);
+	unsigned_key = malloc(sizeof(unsigned char) * strlen(key));
+	index = key_index(unsigned_key, ht->size);
+	free(unsigned_key);
 	new_element = create_element(key, value);
+	if (!new_element)
+		return (0);
+	current_element = ht->array[index];
+	if (current_element)
+		new_element->next = ht->array[index];
 
+	ht->array[index] = new_element;
 	return (1);
 }

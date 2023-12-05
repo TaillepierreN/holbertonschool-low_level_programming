664450d221afdeea1c982aedf638cfa325d70da1
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2023-12-05 14:15:34 +0100

    2-key_index added space

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -12,7 +12,7 @@ unsigned long int key_index(const unsigned char *key, unsigned long int size)
 	unsigned long int index;
 
 	if (!key || size == 0)
-		return(0);
+		return (0);
 	index = abs(hash_djb2(key) % size);
 
 	return (index);

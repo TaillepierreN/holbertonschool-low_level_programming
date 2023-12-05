12affc22120f6a24e28879f954fa510767890a2b
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2023-12-05 10:03:09 +0100

    created hash_tables.h 0-hash_table_create and readme

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,18 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - create a hash table
+ * @size: size of the array
+ * Return: pointer to the newly created hash table
+*/
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *hash_table = malloc(sizeof(hash_table_t));
+	if (!hash_table)
+		return (NULL);
+	hash_table->size = size;
+	hash_table->array = calloc(hash_table->size, sizeof(hash_node_t));
+	if (!hash_table->array)
+		return(NULL);
+	return (hash_table);
+}

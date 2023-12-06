57f83cd22ebe8686bfb3a5002f99a48bbe1f0ab7
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2023-12-06 09:10:33 +0100

    betty compliance

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -1,12 +1,11 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_set - add an element to the hash table
- * @ht: hash table where to add or update the element
- * @key: key of the element (string)
- * @value: value associated with the key (duplicated)
- * Return: 1 on success, else 0
- */
+ * create_element - create an element to add to the hash table
+ * @key: key of the element
+ * @value: value of the element
+ * Return: new element created
+*/
 hash_node_t *create_element(const char *key, const char *value)
 {
 	hash_node_t *element;
@@ -28,7 +27,13 @@ hash_node_t *create_element(const char *key, const char *value)
 	element->next = NULL;
 	return (element);
 }
-
+/**
+ * hash_table_set - add an element to the hash table
+ * @ht: hash table where to add or update the element
+ * @key: key of the element (string)
+ * @value: value associated with the key (duplicated)
+ * Return: 1 on success, else 0
+ */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;

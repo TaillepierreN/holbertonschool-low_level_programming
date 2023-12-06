dad46d6fc05aec4302ccc6d50655e9f41d0ac01e
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2023-12-06 15:59:10 +0100

    3-hash_table_set test

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -13,7 +13,7 @@ hash_node_t *create_element(const char *key, const char *value)
 	element = malloc(sizeof(hash_node_t));
 	if (!element)
 		return (NULL);
-	element->key = strdup(key);
+	element->key = key;
 	if (!element->key)
 	{
 		free(element);
@@ -37,7 +37,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	char *dupeValue;
 	hash_node_t *new_element, *current_element;
 
-	if (key[0] == '\0' || key == NULL || !ht || !value)
+	if (!key || !ht || !value)
 		return (0);
 	unsigned_key = malloc(sizeof(unsigned char) * strlen(key));
 	memcpy(unsigned_key, key, strlen(key) + 1);
@@ -47,22 +47,25 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (!dupeValue)
 		return (0);
 	current_element = ht->array[index];
-	if (current_element)
-	{
+
 		while (current_element->next != NULL)
 		{
 			if (strcmp(current_element->key, key) == 0)
 			{
 				free(current_element->value);
 				current_element->value = strdup(dupeValue);
+				free(dupeValue);
 				return (1);
 			}
 			current_element = current_element->next;
 		}
-	}
+
 	new_element = create_element(key, dupeValue);
 	if (!new_element)
+	{
+		free(dupeValue);
 		return (0);
+	}
 	new_element->next = ht->array[index];
 	ht->array[index] = new_element;
 	return (1);

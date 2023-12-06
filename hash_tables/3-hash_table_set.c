10cff1ff99a56809cde1559eb7573d0f938cba53
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2023-12-06 15:13:52 +0100

    change malloc/strcpy to strdup, copied value into dupeValue

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -13,8 +13,8 @@ hash_node_t *create_element(const char *key, const char *value)
 	element = malloc(sizeof(hash_node_t));
 	if (!element)
 		return (NULL);
-	element->key = malloc(strlen(key) + 1);
-	element->value = malloc(strlen(value) + 1);
+	element->key = strdup(key);
+	element->value = strdup(value);
 	if (!element->key || !element->value)
 	{
 		free(element->key);
@@ -22,8 +22,6 @@ hash_node_t *create_element(const char *key, const char *value)
 		free(element);
 		return (NULL);
 	}
-	strcpy(element->key, key);
-	strcpy(element->value, value);
 	element->next = NULL;
 	return (element);
 }
@@ -38,6 +36,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
 	unsigned char *unsigned_key;
+	char *dupeValue;
 	hash_node_t *new_element, *current_element;
 
 	if (key[0] == '\0' || key == NULL || !ht || !value)
@@ -46,7 +45,10 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	memcpy(unsigned_key, key, strlen(key) + 1);
 	index = key_index(unsigned_key, ht->size);
 	free(unsigned_key);
-	new_element = create_element(key, value);
+	dupeValue = strdup(value);
+	if (!dupeValue)
+		return (0);
+	new_element = create_element(key, dupeValue);
 	if (!new_element)
 		return (0);
 	current_element = ht->array[index];
@@ -57,7 +59,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 			if (strcmp(current_element->key, key) == 0)
 			{
 				free(current_element->value);
-				current_element->value = strdup(value);
+				current_element->value = dupeValue;
 				free(new_element->key);
 				free(new_element->value);
 				free(new_element);

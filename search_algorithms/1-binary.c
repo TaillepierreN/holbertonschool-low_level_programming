7e00187b1bfec8e48cc2b9266b442140aff409e4
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2024-01-16 13:59:09 +0100

    1-binary add a return -1 if no array

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -11,28 +11,28 @@ int binary_search(int *array, size_t size, int value)
 {
 	unsigned int low = 0, medium, high = size - 1, index;
 
-	if (array)
+	if (!array)
+		return (-1);
+
+	while (low <= high)
 	{
-		while (low <= high)
+		printf("searching in array: ");
+		for (index = low; index <= high; index++)
 		{
-
-			printf("searching in array: ");
-			for (index = low; index <= high; index++)
-			{
-				printf("%d", array[index]);
-				if (index < high)
-					printf(", ");
-			}
-			printf("\n");
-
-			medium = low + (high - low) / 2;
-			if (array[medium] == value)
-				return (medium);
-			if (array[medium] < value)
-				low = medium + 1;
-			else
-				high = medium - 1;
+			printf("%d", array[index]);
+			if (index < high)
+				printf(", ");
 		}
+		printf("\n");
+
+		medium = low + (high - low) / 2;
+		if (array[medium] == value)
+			return (medium);
+		if (array[medium] < value)
+			low = medium + 1;
+		else
+			high = medium - 1;
 	}
+
 	return (-1);
 }

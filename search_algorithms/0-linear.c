a92d6970e36718f6f0ddc70707be35d12fed6fd7
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2024-01-16 10:35:43 +0100

    Added 0-linear.c

---
+++ search_algorithms/0-linear.c
@@ -0,0 +1,17 @@
+#include "search_algos.h"
+
+int linear_search(int *array, size_t size, int value)
+{
+	unsigned int index;
+
+	if (array)
+	{
+		for (index = 0; index < size; index++)
+		{
+			printf("Value checked array[%d] = [%d]\n", index, array[index]);
+			if (array[index] == value)
+				return (index);
+		}
+	}
+	return (-1);
+}
